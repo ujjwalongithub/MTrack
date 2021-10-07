c5967a5b1f27ba50ff8565c35277ddd00617c7cf
Ujjwal <ujjwal@mlresearcher.onmicrosoft.com>
2021-10-07 15:59:18 +0200

    Added reading of gt.txt

--- utils/mot17_tfrecords.py
+++ utils/mot17_tfrecords.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
-import tensorflow_io as tfio
 import os
 import glob
 import argparse
 import configparser
 import typing
+import pandas as pd
+from loguru import logger
 
 parser = argparse.ArgumentParser(prog='Code to create TFRecords for MOT17.')
 parser.add_argument('--mot_dir', help='Path storing MOT17 dataset.')
@@ -56,14 +57,69 @@ def _bytes_list_feature(values):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=values))
 
 
-@tf.function
-def read_video(video_file_name: str):
-    file_content = tf.io.read_file(
-        filename=video_file_name,
-        name='video_reader'
+def get_images(seq_folder: str) -> typing.List[str]:
+    img_folder = os.path.join(
+        seq_folder,
+        'img1'
     )
-    return tfio.experimental.ffmpeg.decode_video(
-        content=file_content,
-        index=0,
-        name='video_decoder'
+    img_extension = get_mot_seq_info(seq_folder)['imExt']
+
+    images = glob.glob(
+        os.path.join(
+            img_folder,
+            '*{}'.format(img_extension)
+        )
     )
+
+    images.sort(
+        key=lambda x: int(os.path.splitext(os.path.basename(x))[0])
+    )
+
+    return images
+
+
+def chunk_images(image_list: typing.List[str], num_frames: int) -> \
+        typing.List[typing.List[
+            str]]:
+    chunks = [image_list[i:i + num_frames] for i in range(0, len(image_list),
+                                                          num_frames)]
+
+    for chunk in chunks:
+        if len(chunk) != num_frames:
+            chunks.remove(chunk)
+
+    return chunks
+
+
+def read_gt(seq_folder: str):
+    gt_file = os.path.join(
+        seq_folder,
+        'gt', 'gt.txt'
+    )
+
+    df = pd.read_csv(
+        gt_file,
+        sep=',',
+        names=['frame_id', 'tracking_id', 'xmin', 'ymin', 'width', 'height',
+               'ignore_zero', 'class_id', 'confidence'],
+        engine='c'
+    )
+
+    logger.debug('Initial number of rows = {}.'.format(df.shape[0]))
+    df = df[df.ignore_zero != 0]
+
+    logger.debug('Number of rows after ignoring bad GT = {}.'.format(
+        df.shape[0]))
+
+    df = df[df.tracking_id >= 0]
+
+    logger.debug('Number of rows after removing negative tracking IDs = {'
+                 '}.'.format(df.shape[0]))
+
+    df = df[df.class_id == 1]
+
+    logger.debug('Number of rows after retaining only pedestrians = {'
+                 '}.'.format(df.shape[0]))
+
+    df_grp = df.groupby(['frame_id'], sort=True)
+    return df_grp
