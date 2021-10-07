5a2b378ea1e03fe289aec7f86ecd55351e625300
Ujjwal <ujjwal@mlresearcher.onmicrosoft.com>
2021-10-07 17:45:35 +0200

    Added Bottleneck layer and converted each layer to a tf.keras.layers.Layer

--- backbones/r3d.py
+++ backbones/r3d.py
@@ -26,7 +26,7 @@ def conv1x1x1(
     )
 
 
-class BasicBlock(tf.Module):
+class BasicBlock(tf.keras.layers.Layer):
     def __init__(self, num_filters, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3x3(num_filters, stride)
@@ -40,14 +40,52 @@ class BasicBlock(tf.Module):
         self.downsample = downsample
         self.stride = stride
 
-    def __call__(self, x, training=False):
+    def call(self, x):
         residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
 
-        out = self.conv1(x, training=training)
-        out = self.bn1(out, training=training)
-        out = self.relu(out, training=training)
-        out = self.conv2(out, training=training)
-        out = self.bn2(out, training=training)
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(tf.keras.layers.Layer):
+    def __init__(self,
+                 num_filters,
+                 stride=1,
+                 downsample=None
+                 ):
+        super(Bottleneck, self).__init__()
+        self.conv1 = conv1x1x1(num_filters)
+        self.bn1 = layers.BatchNormalization(axis=1, fused=True)
+        self.conv2 = conv3x3x3(num_filters, stride)
+        self.bn2 = layers.BatchNormalization(axis=1, fused=True)
+        self.conv3 = conv1x1x1(num_filters * 4)
+        self.bn3 = layers.BatchNormalization(axis=1, fused=True)
+        self.relu = layers.ReLU()
+        self.downsample = downsample
+        self.stride = stride
+
+    def call(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
